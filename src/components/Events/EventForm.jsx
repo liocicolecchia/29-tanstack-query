f7a8759021e2a8572136a2cdec38f82cc95642cd
Lio <liocicolecchia@gmail.com>
2026-07-08 00:23:04 +0200

    adding a new fetch request to http.js to fetch the selectable images for a new event. Displaying the images if data is fetched, displaying text wile isPending, displaying error if isError

--- src/components/Events/EventForm.jsx
+++ src/components/Events/EventForm.jsx
@@ -1,9 +1,17 @@
-import { useState } from 'react';
+import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 
-import ImagePicker from '../ImagePicker.jsx';
+import ImagePicker from "../ImagePicker.jsx";
+import { fetchSelectableImages } from "../../util/http.js";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function EventForm({ inputData, onSubmit, children }) {
   const [selectedImage, setSelectedImage] = useState(inputData?.image);
+
+  const { data, isPending, isError } = useQuery({
+    queryKey: ["events-images"],
+    queryFn: fetchSelectableImages,
+  });
 
   function handleSelectImage(image) {
     setSelectedImage(image);
@@ -26,46 +34,45 @@ export default function EventForm({ inputData, onSubmit, children }) {
           type="text"
           id="title"
           name="title"
-          defaultValue={inputData?.title ?? ''}
+          defaultValue={inputData?.title ?? ""}
         />
       </p>
 
-      <div className="control">
-        <ImagePicker
-          images={[]}
-          onSelect={handleSelectImage}
-          selectedImage={selectedImage}
+      {isPending && <p>Loading selectable images...</p>}
+      {isError && (
+        <ErrorBlock
+          title="Failed to load selectable images."
+          message="Please try again later."
         />
-      </div>
+      )}
+      {data && (
+        <div className="control">
+          <ImagePicker
+            images={data}
+            onSelect={handleSelectImage}
+            selectedImage={selectedImage}
+          />
+        </div>
+      )}
 
       <p className="control">
         <label htmlFor="description">Description</label>
         <textarea
           id="description"
           name="description"
-          defaultValue={inputData?.description ?? ''}
+          defaultValue={inputData?.description ?? ""}
         />
       </p>
 
       <div className="controls-row">
         <p className="control">
           <label htmlFor="date">Date</label>
-          <input
-            type="date"
-            id="date"
-            name="date"
-            defaultValue={inputData?.date ?? ''}
-          />
+          <input type="date" id="date" name="date" defaultValue={inputData?.date ?? ""} />
         </p>
 
         <p className="control">
           <label htmlFor="time">Time</label>
-          <input
-            type="time"
-            id="time"
-            name="time"
-            defaultValue={inputData?.time ?? ''}
-          />
+          <input type="time" id="time" name="time" defaultValue={inputData?.time ?? ""} />
         </p>
       </div>
 
@@ -75,7 +82,7 @@ export default function EventForm({ inputData, onSubmit, children }) {
           type="text"
           id="location"
           name="location"
-          defaultValue={inputData?.location ?? ''}
+          defaultValue={inputData?.location ?? ""}
         />
       </p>
 
