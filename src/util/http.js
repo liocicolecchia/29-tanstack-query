af2b48700e81c17c1683118aecd09f956cef2576
Lio <liocicolecchia@gmail.com>
2026-07-08 00:33:32 +0200

    outsourcing the creation of queryClient from App.jsx to http.js so that i can use it in multiple components. in new event adding a onSuccess: to useMutation in NewEvent.jsx. in onSuccess calling queryClient.invalidateQueries to trigger a refatching of the events so that the added event shows up imediatly. also navigate back to /events

--- src/util/http.js
+++ src/util/http.js
@@ -1,3 +1,7 @@
+import { QueryClient } from "@tanstack/react-query";
+
+export const queryClient = new QueryClient();
+
 export async function fetchEvents({ signal, searchTerm }) {
   console.log(searchTerm);
 
